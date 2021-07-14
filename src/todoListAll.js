4fa1761ec05bc24d60aefc6e5a9b2777331b3d56
Patryk Siewiera <45758580+patryk-siewiera@users.noreply.github.com>
2021-07-14 11:33:18 +0200

    feat: print list in boxes

--- src/todoListAll.js
+++ src/todoListAll.js
@@ -15,8 +15,7 @@ export default function TodoListAll() {
 		<div>
 			<div className="todoList">ToDo-List</div>
 			<div className="logout">logout</div>
-			<div>
-				{" "}
+			<div className="maxWidthList">
 				<input
 					{...register("search", {})}
 					placeholder="Search"
@@ -27,19 +26,19 @@ export default function TodoListAll() {
 					<option value="AZ">A&lt;-Z alfabetycznie</option>
 					<option value="ZA">Z-&gt;A</option>
 				</select>
-			</div>
-			<div className="text-white mt-44 max-w-2xl">
-				{/* {mockData.map((element) => (
+				<div className="text-white mt-44 ">
+					{/* {mockData.map((element) => (
 					<div>{element}</div>
 				))} */}
-				{/* {JSON.stringify(mockData[1].task[0])} */}
-				{Object.entries(mockData).map((item) => (
-					<div>
-						{" "}
-						{JSON.stringify(item)} <br />
-						<br />
-					</div>
-				))}
+					{/* {JSON.stringify(mockData[1].task[0])} */}
+					<ul>
+						{Object.entries(mockData).map((item) => (
+							<div>
+								<li>{JSON.stringify(item)} </li>
+							</div>
+						))}
+					</ul>
+				</div>
 			</div>
 		</div>
 	);
