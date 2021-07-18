0ca21e0a41772feb6c12150401777b172048e40a
Patryk Siewiera <45758580+patryk-siewiera@users.noreply.github.com>
2021-07-18 19:12:32 +0200

    feat: init style of details

--- src/detailsModal.js
+++ src/detailsModal.js
@@ -1,6 +1,15 @@
 import React from "react";
 import "./detailsModal.css";
 
+function mapAllTasks() {
+	return (
+		<div className="oneTaskGrid">
+			<input type="checkbox" className="checkboxOneTask" />
+			<input type="text" placeholder="Task Name" className="taskName" />
+		</div>
+	);
+}
+
 export default function DetailsModal(data) {
 	return (
 		<div className="overlayWindow">
@@ -12,8 +21,13 @@ export default function DetailsModal(data) {
 				/>
 			</div>
 			<div className="horizontalLine" />
-			<div> map of tasks</div>
-			this is details modal
+			MAP HERE ALL TASKS
+			<div className="allTasks">
+				{mapAllTasks()}
+				{mapAllTasks()}
+				{mapAllTasks()}
+				{mapAllTasks()}
+			</div>
 			<div className="buttonCancelAdd">
 				<button className="cancelSmall">CANCEL</button>
 				<button className="addSmall">ADD</button>
